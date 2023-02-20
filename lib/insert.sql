ec3c1b15b89e2bd49569f82afca73510c272900e
Tracy Mumbi <tracy.mumbi@student.moringaschool.com>
2023-02-20 09:54:00 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES
+("Mr.Chocolate", 23, "M", "blue", "mild", true),
+("Rowdy", 23, "F", "orange", "mild", false),
+("Tabitha", 13, "M", "black", "medium", false),
+("Sergeant Brown",12,"F","maroon","aggressive",false),
+("Melissa", 21, "M", "lime", "defensive", true),
+("Grinch", 14, "F", "gold", "mild", false),
+("Wendy", 34, "M", "violet", "aggressive", false),
+(NULL, 26, "F", "firebrick", "mild", true);
